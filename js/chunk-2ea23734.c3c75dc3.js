1b3cc0e2cb235fd09ded61ef5acd847161da244e
nkayp <nkayp2049@gmail.com>
2021-11-23 12:23:19 -0600

    add bookmarks

---
+++ js/chunk-2ea23734.c3c75dc3.js
@@ -0,0 +1,91 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["chunk-2ea23734"],{
+
+/***/ "a325":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony import */ var _node_modules_mini_css_extract_plugin_dist_loader_js_ref_8_oneOf_1_0_node_modules_css_loader_dist_cjs_js_ref_8_oneOf_1_1_node_modules_vue_loader_v16_dist_stylePostLoader_js_node_modules_postcss_loader_src_index_js_ref_8_oneOf_1_2_node_modules_sass_loader_dist_cjs_js_ref_8_oneOf_1_3_node_modules_cache_loader_dist_cjs_js_ref_0_0_node_modules_vue_loader_v16_dist_index_js_ref_0_1_Bookmarks_vue_vue_type_style_index_0_id_2321e4a6_scoped_true_lang_scss__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__("d725");
+/* harmony import */ var _node_modules_mini_css_extract_plugin_dist_loader_js_ref_8_oneOf_1_0_node_modules_css_loader_dist_cjs_js_ref_8_oneOf_1_1_node_modules_vue_loader_v16_dist_stylePostLoader_js_node_modules_postcss_loader_src_index_js_ref_8_oneOf_1_2_node_modules_sass_loader_dist_cjs_js_ref_8_oneOf_1_3_node_modules_cache_loader_dist_cjs_js_ref_0_0_node_modules_vue_loader_v16_dist_index_js_ref_0_1_Bookmarks_vue_vue_type_style_index_0_id_2321e4a6_scoped_true_lang_scss__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_node_modules_mini_css_extract_plugin_dist_loader_js_ref_8_oneOf_1_0_node_modules_css_loader_dist_cjs_js_ref_8_oneOf_1_1_node_modules_vue_loader_v16_dist_stylePostLoader_js_node_modules_postcss_loader_src_index_js_ref_8_oneOf_1_2_node_modules_sass_loader_dist_cjs_js_ref_8_oneOf_1_3_node_modules_cache_loader_dist_cjs_js_ref_0_0_node_modules_vue_loader_v16_dist_index_js_ref_0_1_Bookmarks_vue_vue_type_style_index_0_id_2321e4a6_scoped_true_lang_scss__WEBPACK_IMPORTED_MODULE_0__);
+/* unused harmony reexport * */
+
+
+/***/ }),
+
+/***/ "b51b":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+// ESM COMPAT FLAG
+__webpack_require__.r(__webpack_exports__);
+
+// EXTERNAL MODULE: ./node_modules/vue/dist/vue.runtime.esm-bundler.js + 3 modules
+var vue_runtime_esm_bundler = __webpack_require__("7a23");
+
+// CONCATENATED MODULE: ./node_modules/cache-loader/dist/cjs.js??ref--12-0!./node_modules/thread-loader/dist/cjs.js!./node_modules/babel-loader/lib!./node_modules/vue-loader-v16/dist/templateLoader.js??ref--6!./node_modules/pug-plain-loader!./node_modules/cache-loader/dist/cjs.js??ref--0-0!./node_modules/vue-loader-v16/dist??ref--0-1!./src/views/Bookmarks.vue?vue&type=template&id=2321e4a6&scoped=true&lang=pug
+
+
+var _withId = /*#__PURE__*/Object(vue_runtime_esm_bundler["M" /* withScopeId */])("data-v-2321e4a6");
+
+Object(vue_runtime_esm_bundler["y" /* pushScopeId */])("data-v-2321e4a6");
+
+var _hoisted_1 = {
+  class: "page"
+};
+
+var _hoisted_2 = /*#__PURE__*/Object(vue_runtime_esm_bundler["g" /* createStaticVNode */])("<div class=\"columns\" data-aos=\"fade-up\" data-v-2321e4a6><a class=\"column sub-section m-4 my-link sub-section-header is-size-5\" href=\"https://www.svgbackgrounds.com/\" target=\"tabnew\" data-v-2321e4a6>SVGBackgrounds.com<p class=\"text pt-4 is-size-6\" data-v-2321e4a6>This is a convenient site that gives you a bunch of template svg backgrounds. The background this website uses is actually a decently modified version of the endless constellation background.</p></a><a class=\"column sub-section m-4 my-link sub-section-header is-size-5\" href=\"https://countapi.xyz/\" target=\"tabnew\" data-v-2321e4a6>CountAPI.xyz<p class=\"text pt-4 is-size-6\" data-v-2321e4a6>This is a free to use api that counts how many times your website, or even each page on your website, has been visited.</p></a><a class=\"column sub-section m-4 my-link sub-section-header is-size-5\" href=\"https://excalidraw.com/\" target=\"tabnew\" data-v-2321e4a6>excalidraw.com<p class=\"text pt-4 is-size-6\" data-v-2321e4a6>This is a cool website that allows you to draw sketches in your browser and then save or share them.</p></a></div>", 1);
+
+Object(vue_runtime_esm_bundler["w" /* popScopeId */])();
+
+var render = /*#__PURE__*/_withId(function (_ctx, _cache, $props, $setup, $data, $options) {
+  var _component_Header = Object(vue_runtime_esm_bundler["C" /* resolveComponent */])("Header");
+
+  return Object(vue_runtime_esm_bundler["v" /* openBlock */])(), Object(vue_runtime_esm_bundler["e" /* createBlock */])("div", _hoisted_1, [Object(vue_runtime_esm_bundler["i" /* createVNode */])(_component_Header, {
+    title: "Bookmarks 📚",
+    description: "Here are some cool things I've stumbled across on the web"
+  }), _hoisted_2]);
+});
+// CONCATENATED MODULE: ./src/views/Bookmarks.vue?vue&type=template&id=2321e4a6&scoped=true&lang=pug
+
+// EXTERNAL MODULE: ./src/components/Header.vue + 4 modules
+var Header = __webpack_require__("0418");
+
+// CONCATENATED MODULE: ./node_modules/cache-loader/dist/cjs.js??ref--12-0!./node_modules/thread-loader/dist/cjs.js!./node_modules/babel-loader/lib!./node_modules/cache-loader/dist/cjs.js??ref--0-0!./node_modules/vue-loader-v16/dist??ref--0-1!./src/views/Bookmarks.vue?vue&type=script&lang=js
+
+/* harmony default export */ var Bookmarksvue_type_script_lang_js = ({
+  name: 'Dotfiles',
+  components: {
+    Header: Header["a" /* default */]
+  },
+  data: function data() {
+    return {};
+  },
+  methods: {},
+  mounted: function mounted() {}
+});
+// CONCATENATED MODULE: ./src/views/Bookmarks.vue?vue&type=script&lang=js
+ 
+// EXTERNAL MODULE: ./src/views/Bookmarks.vue?vue&type=style&index=0&id=2321e4a6&scoped=true&lang=scss
+var Bookmarksvue_type_style_index_0_id_2321e4a6_scoped_true_lang_scss = __webpack_require__("a325");
+
+// CONCATENATED MODULE: ./src/views/Bookmarks.vue
+
+
+
+
+
+Bookmarksvue_type_script_lang_js.render = render
+Bookmarksvue_type_script_lang_js.__scopeId = "data-v-2321e4a6"
+
+/* harmony default export */ var Bookmarks = __webpack_exports__["default"] = (Bookmarksvue_type_script_lang_js);
+
+/***/ }),
+
+/***/ "d725":
+/***/ (function(module, exports, __webpack_require__) {
+
+// extracted by mini-css-extract-plugin
+
+/***/ })
+
+}]);
+//# sourceMappingURL=chunk-2ea23734.c3c75dc3.js.map
